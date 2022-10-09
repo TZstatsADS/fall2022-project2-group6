afdb10482ba148030bc6d6c3c544f4e985eafd8f
yg2804 <98439644+yg2804@users.noreply.github.com>
2022-10-08 20:19:46 -0400

    Add files via upload

--- lib/server_ui_COVID_info.R
+++ lib/server_ui_COVID_info.R
@@ -4,13 +4,13 @@ library(tidyverse)
 library(zoo)
 
 covid <- read_csv("covid_hosp\\COVID-19_Daily_Counts_of_Cases__Hospitalizations__and_Deaths.csv")
-
+head(covid)
 
 server <- function(input, output) {
   
   
   output$plot=renderPlot({
-    covid_date <- covid %>% mutate(date_of_interest = as.Date(date_of_interest, "%m/%d/%Y")) %>%
+    covid_date <- covid %>% mutate(date_of_interest = as.POSIXct(date_of_interest, format="%m/%d/%Y")) %>%
       select(date_of_interest, CASE_COUNT, HOSPITALIZED_COUNT, DEATH_COUNT) %>%
       filter(date_of_interest >= as.Date(input$dates[1]) & date_of_interest <= as.Date(input$dates[2])) %>%
       pivot_longer(cols=c(`CASE_COUNT`, `HOSPITALIZED_COUNT`, `DEATH_COUNT`), names_to = "variable", values_to = "count")
@@ -18,7 +18,6 @@ server <- function(input, output) {
     
     ggplot(data=covid_date, aes(x=date_of_interest, y=count)) + 
       geom_line(aes(color=variable)) + 
-      scale_x_date(date_breaks  ="2 month") +
       labs(x="Date of Interest", 
            y="Count") +
       scale_colour_manual(values=c("darkcyan", "black", "coral")) +
